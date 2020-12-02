b963578bdfe944eb5aa31ebe54023f83a031c497
Mazen Omar <73177229+meorules@users.noreply.github.com>
2020-12-02 20:47:38 +0400

    Add files via upload

---
+++ MeetingScheduler/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace MeetingScheduler
+{
+  static class Program
+  {
+    /// <summary>
+    /// The main entry point for the application.
+    /// </summary>
+    [STAThread] 
+    static void Main()
+    {
+      Meeting[,] MeetingSlots = new Meeting[6, 4];
+      //Application.EnableVisualStyles();
+      //Application.SetCompatibleTextRenderingDefault(false);
+      //Application.Run(new Form1());
+      char[] meetings = { 'a','c','d'};
+      User testUser = new User("Mazen","Omar", User.EmployeeType.systemAdmin,meetings, meetings, meetings);
+      User testUser2 = new User("Mazen", "Omar", User.EmployeeType.systemAdmin, meetings, meetings, meetings);
+      User testUser33 = new User("Mazen", "Omar", User.EmployeeType.systemAdmin, meetings, meetings, meetings);
+
+            
+
+
+      Console.WriteLine("hello world");
+
+            
+    }
+  }
+}
---
+++ MeetingScheduler/User.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+namespace MeetingScheduler
+{
+  class User
+  {
+    private static int noOfUsers = 0;
+    private int userID;
+    private String firstName;
+    private String lastName;
+    public enum EmployeeType { systemAdmin = 0, equipmentKeeper = 1, meetingInitiator = 2, participant = 3 }
+    private EmployeeType EmpType;
+    private List<char> meetingList;
+    private List<char> exclustionSet;
+    private List<char> preferenceSet;
+    public User(string fname, string lName, EmployeeType empType, char[] meetings, char[] exclude, char[] preference)
+    {
+      userID = noOfUsers++;
+      firstName = fname;
+      lastName = lName;
+      EmpType = empType;
+
+      meetingList = new List<char>();
+      foreach (char c in meetings)
+      {
+                this.meetingList.Add(c);
+      }
+      exclustionSet= new List<char>();
+      foreach (char c in exclude)
+      {
+        this.exclustionSet.Add(c);
+
+      }
+      preferenceSet = new List<char>();
+      foreach (char c in preference)
+      {
+        this.preferenceSet.Add(c);
+      }
+    }
+
+  }
+}
